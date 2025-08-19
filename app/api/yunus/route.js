b8b39130461ef8e56a7b47aca2fd19c75de449fa
xixiqo <yazhar363@gmail.com>
2025-08-19 12:39:00 +0700

    Refactor MongoDB connection to use environment variable and improve error handling

--- app/api/yunus/route.js
+++ app/api/yunus/route.js
@@ -1,15 +1,14 @@
 import { NextResponse } from "next/server";
-import clientPromise from "@/lib/mongodb";
+import { connectToDatabase } from "@/lib/mongodb";
 
 export async function GET() {
   try {
-    const client = await clientPromise;
-    const db = client.db("todo-crud-2"); // ganti sesuai nama DB kamu
-    const students = await db.collection("students").find().toArray();
+    const client = await connectToDatabase();
+    const db = client.db("todo"); // ganti sesuai nama db kamu
+    const data = await db.collection("tasks").find().toArray();
 
-    return NextResponse.json(students);
-  } catch (err) {
-    console.error("❌ Error GET:", err);
-    return NextResponse.json({ message: "Internal Server Error" }, { status: 500 });
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
   }
 }
