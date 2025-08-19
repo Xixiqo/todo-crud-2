7c3260f1d32af0212219270cd4ac5411917013a9
xixiqo <yazhar363@gmail.com>
2025-08-19 12:57:45 +0700

    Remove unnecessary module.exports and clean up next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,9 @@
 import type { NextConfig } from "next";
 
-/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
 };
-
-module.exports = nextConfig;
 
 export default nextConfig;
